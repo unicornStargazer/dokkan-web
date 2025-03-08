f444e1967b7adaaad2a162afc812f4ac200592e4
stargazer <837427104@qq.com>
2025-03-08 17:05:05 +0800

    初始化项目

---
+++ dokkan-starter/src/main/java/com/hb/dokkan/starter/DokkanApplication.java
@@ -0,0 +1,17 @@
+package com.hb.dokkan.starter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(value = "com.hb.dokkan.db.mapper.*")
+public class DokkanApplication {
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(DokkanApplication.class,args);
+        } catch (Exception e) {
+            throw new RuntimeException("spring启动错误，error:{}" + e.getMessage());
+        }
+    }
+}
