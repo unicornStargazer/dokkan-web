0cc3527cf1f976fd6403b053ec945ccbbdde5592
stargazer <837427104@qq.com>
2025-12-15 00:40:43 +0800

    feat-项目结构调整

--- dokkan-api/src/main/java/com/hb/dokkan/api/cards/DokkanCardApi.java
+++ dokkan-api/src/main/java/com/hb/dokkan/api/cards/DokkanCardApi.java
@@ -7,10 +7,7 @@ import com.hb.dokkan.common.domain.PageResponse;
 import com.hb.dokkan.service.DokkanCardDelegate;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Description card api
